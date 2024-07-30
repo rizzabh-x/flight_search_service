777c8964b68dd744e36a54fdec73a322d969a494
Rishabh <rishabhmalav9@gmail.com>
2024-07-30 12:21:39 +0530

    added flights create crud

---
+++ src/services/flight-service.js
@@ -0,0 +1,32 @@
+const {FlightRepository, AirplaneRepository} = require('../repository/index');
+const {compareTime} = require('../utils/helper');
+
+class FlightService {
+
+    constructor() {
+        this.airplaneRepository = new AirplaneRepository();
+        this.flightrepository = new FlightRepository();
+    }
+
+    async createFlight(data){
+        try {
+            if(!compareTime(data.arrivalTime, data.departureTime)){
+                throw {error: 'Arrival Time should not be less than departure time'};
+            }
+            const airplane = await this.airplaneRepository.getAirplane(data.airplaneId);
+            const flight = await this.flightrepository.createFlight({
+                ...data, totalSeats:airplane.capacity
+            });
+            return flight;
+        } catch (error) {
+            console.log("Something gone wrong in 1 service layer");
+            throw {error};
+        }
+    }
+
+    async getFlightData() {
+        //todo
+    }
+}
+
+module.exports = FlightService;
